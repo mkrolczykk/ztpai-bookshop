cf75cdcf0b099c62d6bbb7fd149006b9e0431bb1
mkrolczyk <m.krolczyk66@gmail.com>
2023-12-12 23:17:22 +0100

    mkrolczyk - created gateway service; connected gateway with auth service; prepared docker (local env) for gateway and auth services

---
+++ backend/infrastructure/local/scripts/check-gatewayservice-server-started.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# check-gatewayservice-server-started.sh
+
+
+# TODO -> update script to check gateway service health endpoint
+#apt-get update -y
+#
+#yes | apt-get install curl
+#
+#curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://gateway_service_1:8081/health)
+#
+#echo "result status code:" "$curlResult"
+#
+#while [[ ! $curlResult == "200" ]]; do
+#  >&2 echo "Gateway service server is not up yet!"
+#  sleep 2
+#  curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://gateway_service_1:8081/health)
+#done
--- backend/services/book-service/src/main/java/pl/bookshop/bookservice/BookServiceApplication.java
+++ backend/services/book-service/src/main/java/pl/bookshop/bookservice/BookServiceApplication.java
@@ -1,9 +1,17 @@
 package pl.bookshop.bookservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"pl.bookshop.bookservice.*", "pl.bookshop.auth.util.*"})
+@EntityScan(basePackages = {"pl.bookshop.bookservice.*", "pl.bookshop.auth.util.*"})
+@OpenAPIDefinition(
+	servers = {
+		@Server(url = "http://localhost:8081/bookapp", description = "[LOCAL env] API Gateway Bookshop service endpoint"),
+		@Server(url = "http://localhost:8290/bookapp", description = "[LOCAL env] Bookshop service direct endpoint (Default: DISABLED)")})
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
