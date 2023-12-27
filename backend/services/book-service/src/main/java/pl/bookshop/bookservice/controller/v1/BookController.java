42ee4e52c25d69590f057559c917bcbaa3e1f663
mkrolczyk <m.krolczyk66@gmail.com>
2023-12-27 17:58:49 +0100

    mkrolczyk - updated API endpoint for adding new books

--- backend/services/book-service/src/main/java/pl/bookshop/bookservice/controller/v1/BookController.java
+++ backend/services/book-service/src/main/java/pl/bookshop/bookservice/controller/v1/BookController.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import pl.bookshop.auth.util.annotation.AdminAuthority;
+import pl.bookshop.auth.util.annotation.EmployeeAuthority;
 import pl.bookshop.auth.util.annotation.UserAuthority;
 import pl.bookshop.bookservice.dto.request.AddBookReq;
 import pl.bookshop.bookservice.service.BookService;
@@ -17,8 +19,8 @@ class BookController {
 
     private final BookService bookService;
 
-    @UserAuthority
-    @PostMapping
+    @EmployeeAuthority
+    @PostMapping("/add")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void addBook(@RequestBody @Valid AddBookReq bookDto) {
 
